d8fd75f34adbc2e29a7ddf74873a0289f5ebbf91
Davi Silva <davi.silva@code7.com>
2021-03-01 12:01:32 -0300

    updates types.

--- src/controllers/sns/getTopicAttributes.ts
+++ src/controllers/sns/getTopicAttributes.ts
@@ -1,9 +1,7 @@
 import AWS from "../../config/aws/config";
-import { IGetAttributesReturn } from "@interfaces/controllers/sns";
+// import { IGetAttributesReturn } from "@interfaces/controllers/sns";
 
-export default function getTopicAttributes(
-  topicArn: string
-): Promise<IGetAttributesReturn> {
+export default function getTopicAttributes(topicArn: string): Promise<any> {
   return new Promise((resolve, reject) => {
     try {
       const topicAttributes = new AWS.SNS({
